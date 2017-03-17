1734b0ea7a05bd1af30c7206a5bdaabc821d4b38
Tuyet Le <tle83@lsu.edu>
2017-03-17 16:58:02 -0500

    Add Health script

--- Washed Away/Assets/Scripts/Health.cs
+++ Washed Away/Assets/Scripts/Health.cs
@@ -1,16 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public int startHP = 100;	//player's starting hp
+	public int currentHP;		//player's hp after being damaged
+	bool playerHit;
+
+	PlayerMovement playerMovement;
+	GameObject player;
+
+	void Awake()
+	{
+		currentHP = startHP;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerMovement = player.GetComponent <PlayerMovement>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		if (playerHit) {
+			if (currentHP <= 75 && currentHP > 50) {
+				Destroy (GameObject.Find ("Heart4"));
+			}
+			else if (currentHP <= 50 && currentHP > 25) {
+				Destroy (GameObject.Find ("Heart3"));
+			}
+			else if (currentHP <= 25 && currentHP > 0) {
+				Destroy (GameObject.Find ("Heart2"));
+			}
+			else if (currentHP == 0) {
+				Destroy (GameObject.Find ("Heart"));
+				Death ();
+			}
+		}
+		/*
+		if (playerHit && currentHP <= 75) {
+			Destroy (GameObject.Find("Heart4"));
+		}
+		if(playerHit == player && currentHP <= 50) {
+			Destroy (GameObject.Find("Heart3"));
+		}
+		if (playerHit == player && currentHP <= 25) {
+			Destroy (GameObject.Find("Heart2"));
+		}
+		if (playerHit == player && currentHP <= 0) {
+			Destroy (GameObject.Find("Heart"));
+			Death ();
+		}
+		*/
+		Debug.Log (currentHP);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject == player) 
+		{
+			playerHit = true;
+			currentHP -= 25;
+		}
+	}
+
+	void Death ()
+	{
+		playerMovement.enabled = false;		//disable script for player movement
 	}
 }
