21b951e2dd415941cea29803b12cfee220da21fb
Tuyet Le <tle83@lsu.edu>
2017-03-17 17:34:17 -0500

    Add Play Button
    -play button added
    -button will flash white when mouse is held over it

--- Washed Away/Assets/Scripts/ButtonClick.cs
+++ Washed Away/Assets/Scripts/ButtonClick.cs
@@ -4,7 +4,42 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonClick : MonoBehaviour {
-    private void OnMouseDown()
+	
+	public Sprite originalSprite;
+	public Sprite newSprite;
+
+	private SpriteRenderer spriteRenderer;
+
+	bool click;
+
+	void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		if (spriteRenderer.sprite == null) {
+			spriteRenderer.sprite = originalSprite;
+		}
+	}
+
+	void Update()
+	{
+		if(Input.GetMouseButton(0)){
+			SpriteChange ();
+		}
+	}
+
+	void SpriteChange()
+	{
+		if (spriteRenderer.sprite == originalSprite) 
+		{
+			spriteRenderer.sprite = newSprite;
+		} 
+		else 
+		{
+			spriteRenderer.sprite = originalSprite;
+		}
+	}
+
+    private void OnMouseUp()
     {
         SceneManager.LoadScene("Sewer");
         //upon clicking the game object, change the scene
