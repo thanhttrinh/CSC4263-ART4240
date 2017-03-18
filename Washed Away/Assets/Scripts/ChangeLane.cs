fe4161458e05e5af7589e431446ded269e5654dd
Thanh Trinh <trinhtania@gmail.com>
2017-03-17 21:48:05 -0500

    add ChangeLane script and lanetriggers
    - allows the player to transition from one lane to another

---
+++ Washed Away/Assets/Scripts/ChangeLane.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLane : MonoBehaviour {
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Vector3 prevPosition = other.gameObject.transform.position;
+		other.gameObject.transform.position = new Vector3 (prevPosition.x + 5, -4, 0);
+	}
+}
