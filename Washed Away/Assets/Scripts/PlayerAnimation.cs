3c67c27654f0a3a50107055d0f01bcb65b4fa22d
Tuyet Le <tle83@lsu.edu>
2017-03-18 21:21:51 -0500

    Player Animations
    -Working Idle, Running and Jumping state animations

---
+++ Washed Away/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	public Animator player;
+	public KeyCode run;
+	public KeyCode jump;
+	public KeyCode left;
+	public KeyCode right;
+	
+	void Update()
+	{
+		//if W is pressed, PlayerRun animation will play
+		if (Input.GetKeyDown (run)) 
+		{
+			player.SetBool("PlayerRun", true);
+		}
+		//if W is released, PlayerRun animation will stop
+		if (Input.GetKeyUp (run)) {
+			player.SetBool ("PlayerRun", false);
+		}
+
+		if (Input.GetKeyDown (jump)) 
+		{
+			if(Input.GetKeyDown(right))
+			{
+				player.SetTrigger ("PlayerJump");
+			}
+			else if (Input.GetKeyDown (left)) {
+				player.SetTrigger ("PlayerJump");
+			}
+		}
+	}
+}
+
