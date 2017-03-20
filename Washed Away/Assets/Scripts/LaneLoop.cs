b40774c3dc14ce5f7dc02d08bc0a34a7e2d983fb
Tuyet Le <tle83@lsu.edu>
2017-03-20 11:11:07 -0500

    Looping script
    Once the player trigger the lane loop 3 times, the collider will
    disable and let the player transition to the next phase

---
+++ Washed Away/Assets/Scripts/LaneLoop.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneLoop : MonoBehaviour {
+
+	private int counter;
+	private bool enter;
+
+	private BoxCollider2D trigger;
+
+	void Awake()
+	{
+		trigger = gameObject.GetComponent<BoxCollider2D> ();
+	}
+
+	void Update()
+	{
+		//once the player loop 3 times
+		//the the collider will be disable
+		if (enter) {
+			trigger.enabled = false;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		counter++;
+
+		if (counter <= 3) 
+		{
+			Vector3 prevPosition = other.gameObject.transform.position;
+			other.gameObject.transform.position = new Vector3 (prevPosition.x, -4.7f, 0);
+
+			//for testing
+			//other.gameObject.transform.position = new Vector3 (prevPosition.x, 55f, 0);
+
+			enter = false;
+		} 
+		else 
+		{
+			enter = true;
+		}
+
+	}
+}
