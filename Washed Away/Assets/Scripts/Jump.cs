1208f81e93fbc1d3220b58def378fcdf29bc0349
jwemp1 <jrwempe94@gmail.com>
2017-03-20 23:39:18 -0500

    Jump
    Workable jump, will work on more refined solution later

---
+++ Washed Away/Assets/Scripts/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    private bool isJumping = true;
+    public BoxCollider2D waterLane;
+    public Rigidbody2D player;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Jumping());
+        }
+    }
+
+    IEnumerator Jumping()
+    {
+        waterLane.enabled = false;
+        yield return new WaitForSecondsRealtime(0.8f);
+        waterLane.enabled = true;
+    }
+    
+
+	
+}
