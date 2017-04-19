354a1d07ad0b060ac0fb067ef152eef8f94e161c
Thanh Trinh <trinhtania@gmail.com>
2017-04-19 17:42:22 -0500

    Add some falling water animations to sewer scene
    - Update FallingWater script
    - Update WaterTrigger
    - Add FallingWater asset to sewer scene
    
    Falling water still not finished yet

--- Washed Away/Assets/Scripts/FallingWater.cs
+++ Washed Away/Assets/Scripts/FallingWater.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FallingWater : MonoBehaviour {
+	public GameObject runningWater;
+
+	Vector3 previousPosition;
+	Vector3 currentPosition;
 
 	// Use this for initialization
 	void Start () {
-		
+		previousPosition = runningWater.transform.position;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+	}
+
+	// Trigger to reset falling water asset
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.gameObject.tag == "RunningWater") {
+			runningWater.transform.position = previousPosition;
+		}
 	}
 }
