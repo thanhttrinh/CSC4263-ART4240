be05e395338927248fabf128b6e2dc512aa434ee
Thanh Trinh <trinhtania@gmail.com>
2017-04-20 10:37:48 -0500

    edit waterfalltrigger script

--- Washed Away/Assets/Scripts/WaterFallTrigger.cs
+++ Washed Away/Assets/Scripts/WaterFallTrigger.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterFallTrigger : MonoBehaviour {
-	public GameObject water;
-	//public GameObject water2;
-	//public GameObject water3;
+	public GameObject water1;
+	public GameObject water2;
+	public GameObject water3;
+
+	private int counter = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +19,20 @@ public class WaterFallTrigger : MonoBehaviour {
 		
 	}
 	void OnTriggerEnter2D(Collider2D other) {
+		// the fly's warning sign should also be here
+
 		if (other.gameObject.tag == "Player") {
-			water.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
-			water.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0.2f;
+			if (counter == 0) {
+				water1.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+				water1.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0.2f;
+			} else if (counter == 1) {
+				water2.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+				water2.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0.2f;
+			} else {
+				water3.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+				water3.gameObject.GetComponent<Rigidbody2D> ().gravityScale = 0.2f;
+			}
+			counter++;
 		}
 	}
 }
