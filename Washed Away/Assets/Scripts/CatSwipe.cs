ca1430518d0bf1c82fd1e01b3d794ceb9c2d9712
Thanh Trinh <trinhtania@gmail.com>
2017-04-20 20:34:04 -0500

    update catswipe script and sewer scene

--- Washed Away/Assets/Scripts/CatSwipe.cs
+++ Washed Away/Assets/Scripts/CatSwipe.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CatSwipe : MonoBehaviour {
-
+	public GameObject paw;
 	// Use this for initialization
 	void Start () {
 		
@@ -12,5 +12,11 @@ public class CatSwipe : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			paw.gameObject.GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Dynamic;
+		}
 	}
 }
